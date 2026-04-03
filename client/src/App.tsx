684a96248d28a91f7530bc27da61e9a073efb965
Tesla-Turing <pmv965qqhq@privaterelay.appleid.com>
2026-04-03 09:31:58 +0200

    Fallback to home for unknown routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
-import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import Registration from "@/pages/registration";
 import Login from "@/pages/login";
@@ -31,7 +30,7 @@ function Router() {
       <Route path="/dashboard/eleve" component={StudentDashboard} />
       <Route path="/dashboard/parent" component={ParentDashboard} />
       <Route path="/dashboard/professeur" component={TeacherDashboard} />
-      <Route component={NotFound} />
+      <Route component={Home} />
     </Switch>
   );
 }
